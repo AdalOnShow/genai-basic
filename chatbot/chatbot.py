23122d8eac4aec908ca1f686fd5fa4be2ec07dd6
Sharif Adal <sharifadal2008@gmail.com>
2026-07-12 20:20:12 +0600

    feat(chatbot): Replace prompt template with typed message objects

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,21 +1,21 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 
 load_dotenv()
 
 model = ChatGroq(model="llama-3.1-8b-instant", temperature=0.8)
-messages = []
+messages = [SystemMessage(content="you are a funny ai agent")]
 
 print("------------------------Welcome! type 0 to exit chat------------------------")
 while True:
     question = input("You : ")
-    messages.append(question)
+    messages.append(HumanMessage(content=question))
     if question == "0":
         print("Thank you for using our chatbot!")
         break
     response = model.invoke(messages)
-    messages.append(response.content)
+    messages.append(AIMessage(content=response.content))
     print("Bot : ", response.content)
 
 print(messages)
